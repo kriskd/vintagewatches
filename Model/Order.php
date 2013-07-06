04ff36f2a11788265d5ea2981eb08faf28179e10
kris <kris@Ubuntu-VB.(none)>
2013-07-06 09:01:10 -0500

    Model associations

---
+++ Model/Order.php
@@ -0,0 +1,71 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Order Model
+ *
+ * @property Address $Address
+ * @property Watch $Watch
+ */
+class Order extends AppModel {
+
+//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Address' => array(
+			'className' => 'Address',
+			'foreignKey' => 'order_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+                'Watch' => array(
+			'className' => 'Watch',
+			'foreignKey' => 'order_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+
+/**
+ * hasAndBelongsToMany associations
+ *
+ * @var array
+ */
+	/*public $hasAndBelongsToMany = array(
+		'Watch' => array(
+			'className' => 'Watch',
+			'joinTable' => 'orders_watches',
+			'foreignKey' => 'order_id',
+			'associationForeignKey' => 'watch_id',
+			'unique' => 'keepExisting',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+			'deleteQuery' => '',
+			'insertQuery' => ''
+		)
+	);*/
+
+}
