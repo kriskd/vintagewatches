ce5418bdd6cbf0a123a7818edb091a789d2be93d
kris <kris@Ubuntu-VB.(none)>
2013-07-20 15:50:51 -0500

    Method to shorten watch descriptions

---
+++ View/Helper/WatchHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+class WatchHelper extends Helper
+{
+    public function shortDescription($description, $more = null, $limit = 50)
+    {
+        $words = str_word_count($description, 1);
+        if(count($words) > $limit){
+            $slice = array_slice($words, 0, $limit);
+            $ret = h(implode(' ', $slice));
+            if($more != null){
+                return $ret . '... ' . $more;
+            }
+        }
+        return $description;
+    }
+}
