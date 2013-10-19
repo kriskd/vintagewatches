a87c617763013b4d2b26ac6232d273b1865f47b1
kris <kris@Ubuntu-VB.(none)>
2013-10-19 14:56:19 -0500

    Clean-up old code

--- Controller/ImagesController.php
+++ Controller/ImagesController.php
@@ -3,40 +3,13 @@ App::uses('Folder', 'Utility');
 App::uses('File', 'Utility');
 class ImagesController extends Controller
 {
-    public $components = array('ImageUploader' => array('scaledImageTimeStampName' => false));
-    
-    public function admin_picture($id = null)
-    {    
+    public function admin_upload($id = null)
+    {
+        if (!$this->Image->Watch->exists($id)) {
+            $this->redirect(array('controller' => 'watches', 'action' => 'index', $id, 'admin' => true));
+        }
         if ($this->request->is('post') || $this->request->is('put')) {
-            App::uses('Sanitize', 'Utility');
-            $data = Sanitize::clean($this->request->data); 
-            $image = $data[$this->modelClass]['image'];
-            $imagePath = WWW_ROOT . 'files' . DS . $id;
-            $thumbPath = $imagePath . DS . 'thumbs' . DS; 
-            
-            if (!file_exists($imagePath)) {
-                $folder = new Folder($imagePath, true);
-                $thumbFolder = new Folder($thumbPath, true);
-            }
-            
-            $options = array('thumbnail' =>
-                            array('max_width' => 180,
-                                  'max_height' => 100,
-                                  'path' => $thumbPath,
-                                  'custom_name' => $data['Image']['image']['name']
-                                  ),
-                            'max_width' => 700,
-            );
-            
-            try {
-                $output = $this->ImageUploader->upload($image, $imagePath, $options); 
-                $fileName = $this->_getFileNameFromPath($output['file_path']);
-                $data = array('Image' => array('watch_id' => $id, 'filename' => $fileName));
-                $this->Image->save($data);
-                
-            } catch(Exception $e) {
-                $output = array('bool' => FALSE, 'error_message' => $e->getMessage());
-            }
+            $this->Image->save($this->request->data);
         }
         $this->redirect(array('controller' => 'watches', 'action' => 'edit', $id, 'admin' => true));
     }
@@ -66,15 +39,5 @@ class ImagesController extends Controller
         $this->Image->delete($imageId);
         
         $this->redirect($this->referer());
-    }
-    
-    public function admin_upload($id = null)
-    {
-        $this->Image->save($this->request->data);
-        $this->redirect(array('controller' => 'watches', 'action' => 'edit', $id, 'admin' => true));
-    }
-    protected function _getFileNameFromPath($path)
-    {
-        return substr($path, strrpos($path, '/')+1, strlen($path));
     }
 }
