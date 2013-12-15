77ce72ae3ff8567e4e7e8a4dafa08702deadba5a
Kris Dockter <kriskd@gmail.com>
2013-12-15 16:51:32 +0000

    Better check of https

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -133,7 +133,7 @@ class AppController extends Controller {
         $secure = array('orders/checkout', 'orders/totalCart.json', 'orders/getAddress.html', 'orders/getCountry.json', 'orders/checkout.json');
         
         //Redirect to non-secure if https and not on checkout
-        if (prod() == true && (isset($_SERVER['HTTPS']) || $_SERVER['HTTPS']) && !in_array(trim($this->here, '/'), $secure)) {
+        if (prod() == true && (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS'])=='on') && !in_array(trim($this->here, '/'), $secure)) {
 	    $this->redirect('http://' . env('SERVER_NAME') . $this->here);
 	}
         
