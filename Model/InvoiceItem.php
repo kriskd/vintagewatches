cc17997796377f504f47bb7c93fada788cbed685
Kris Dockter <kriskd@gmail.com>
2014-06-01 19:47:58 -0500

    Clean-up

--- Model/InvoiceItem.php
+++ Model/InvoiceItem.php
@@ -34,7 +34,7 @@ class InvoiceItem extends AppModel {
 		'description' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
-				'message' => 'Please enter the item adescription.',
+				'message' => 'Please enter the item description.',
 				//'allowEmpty' => false,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
@@ -50,27 +50,27 @@ class InvoiceItem extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-                        'not_negative' => array(
-                                'rule'    => array('comparison', '>=', 0),
-                                'message' => 'Must be a positive number.'
-                        )
-		),
-	);
+            'not_negative' => array(
+                    'rule'    => array('comparison', '>=', 0),
+                    'message' => 'Must be a positive number.'
+                )
+            ),
+        );
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
-/**
- * belongsTo associations
- *
- * @var array
- */
-	public $belongsTo = array(
-		'Invoice' => array(
-			'className' => 'Invoice',
-			'foreignKey' => 'invoice_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+    /**
+     * belongsTo associations
+     *
+     * @var array
+     */
+    public $belongsTo = array(
+        'Invoice' => array(
+            'className' => 'Invoice',
+            'foreignKey' => 'invoice_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 }
