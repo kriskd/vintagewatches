522e0739ef82bedd9b32a1c1c9b012b33e565e45
Kris Dockter <kriskd@gmail.com>
2014-06-08 06:34:25 -0500

    Test getOrder

--- Test/Case/Model/OrderTest.php
+++ Test/Case/Model/OrderTest.php
@@ -3,6 +3,14 @@ App::Uses('Order', 'Model');
 
 class OrderTest extends CakeTestCase {
     
+    public $fixtures = array(
+        'app.order',
+        'app.watch',
+        'app.image',
+        'app.payment',
+        'app.address'
+    );
+
     public function setUp() {
         parent::setUp();
         $this->Order = ClassRegistry::init('Order');
@@ -19,5 +27,49 @@ class OrderTest extends CakeTestCase {
         ));
         $expected = 425;
         $this->assertEquals($result, $expected);
+    }
+
+    public function testGetOrder() {
+        $result = $this->Order->getOrder(28);
+        $expected = array(
+            'Order' => array(
+                'id' => 28, 
+            ),
+            'Address' => array(
+                    array(
+                        'id' => 94,
+                )
+            ),
+            'Watch' => array(
+                'id' => 44,
+                'Image' => array(
+                    array(
+                        'id' => 289,
+                    ),
+                    array(
+                        'id' => 290,
+                    ),
+                    array(
+                        'id' => 291,
+                    ),
+                    array(
+                        'id' => 292,
+                    ),
+                    array(
+                        'id' => 293,
+                    ),
+                    array(
+                        'id' => 294,
+                    ),
+                    array(
+                        'id' => 295,
+                    ),    
+                ),
+            ),
+            'Payment' => array(
+                'id' => 60,
+            ),
+        );
+        $this->assertEquals($result['Watch']['id'], $expected['Watch']['id']);
     }
 }
