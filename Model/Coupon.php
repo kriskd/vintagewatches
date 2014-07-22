d17a3d2dbf86ce07164a05a59b794af26edccbc9
Kris Dockter <kriskd@gmail.com>
2014-07-21 19:59:23 -0500

    Validate code on create only

--- Model/Coupon.php
+++ Model/Coupon.php
@@ -27,11 +27,11 @@ class Coupon extends AppModel {
 				'allowEmpty' => false,
 				'required' => true,
 				//'last' => false, // Stop validation after this rule
-				'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
             'unique' => array(
                 'rule' => 'uniqueNotArchived',
                 'message' => 'This coupon code is unavailable, choose another.',
+				'on' => 'create', // Limit validation to 'create' or 'update' operations
             ),
 		),
 		'type' => array(
