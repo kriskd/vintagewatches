d3b0442bdf3703c92cf38cd2fd6080e33f65041d
Kris Dockter <kriskd@gmail.com>
2014-07-27 17:17:25 -0500

    Fix afterFind

--- Model/Coupon.php
+++ Model/Coupon.php
@@ -162,11 +162,13 @@ class Coupon extends AppModel {
         }
         unset($value); 
         foreach ($results as &$result) {
-            $result['Coupon']['available'] = $this->available($result);
-            foreach ($result as $key => &$orders) { 
-                if (strcasecmp($key, 'Order')==0) {
-                    foreach($orders as &$order) {
-                       $order = array_merge($orderFields, $order); 
+            if (isset($result[$this->name])) { 
+                $result[$this->name]['available'] = $this->available($result);
+                foreach ($result as $key => &$orders) { 
+                    if (strcasecmp($key, 'Order')==0) {
+                        foreach($orders as &$order) {
+                           $order = array_merge($orderFields, $order); 
+                        }
                     }
                 }
             }
