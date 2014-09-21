47fcdd2e8bdc4f75a6a9b4c9ad3eff1f3e77ffe9
Kris Dockter <kriskd@gmail.com>
2014-09-21 17:51:52 -0500

    Remove old test

--- Test/Case/Model/OrderTest.php
+++ Test/Case/Model/OrderTest.php
@@ -20,34 +20,6 @@ class OrderTest extends FakerTestCase {
         $this->Order = ClassRegistry::init('Order');
     }
     
-    public function testGetSubTotal() {
-        $result = $this->Order->getSubTotal(array(
-            array('Watch' => array(
-                'price' => 250
-            )),
-            array('Watch' => array(
-                'price' => 175
-            )),
-        ));
-        $expected = 425;
-        $this->assertEquals($result, $expected);
-    }
-
-    public function testGetSubTotalBrand() {
-        $result = $this->Order->getSubTotal(array(
-            array('Watch' => array(
-                'price' => 250,
-                'brand_id' => 1,
-            )),
-            array('Watch' => array(
-                'price' => 175,
-                'brand_id' => 2,
-            )),
-        ), 1);
-        $expected = 250;
-        $this->assertEquals($result, $expected);
-    }
-
     public function testGetOrder() {
         
         $result = $this->Order->getOrder(1);
