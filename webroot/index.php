3f70ccb39cea4368fc732698f116b44099baf14b
Kris Dockter <kriskd@gmail.com>
2014-12-14 12:23:03 -0600

    Location of cake core

--- webroot/index.php
+++ webroot/index.php
@@ -62,7 +62,7 @@ if (!defined('APP_DIR')) {
  * The following line differs from its sibling
  * /lib/Cake/Console/Templates/skel/webroot/index.php
  */
-//define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . APP_DIR . DS . 'Lib');
 
 /**
  * Editing below this line should NOT be necessary.
@@ -86,7 +86,7 @@ if (php_sapi_name() == 'cli-server') {
 
 if (!defined('CAKE_CORE_INCLUDE_PATH')) {
 	if (function_exists('ini_set')) {
-		ini_set('include_path', ROOT . DS . 'cakephp' . DS . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
+		ini_set('include_path', ROOT . DS . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
 	}
 	if (!include ('Cake' . DS . 'bootstrap.php')) {
 		$failed = true;
