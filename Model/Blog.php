e7ebb7e7e5707377d4c4c3cf2c0fb4ead468ceda
Kris Dockter <kriskd@gmail.com>
2014-12-23 09:35:43 -0600

    Override paginateCount to get right count

--- Model/Blog.php
+++ Model/Blog.php
@@ -3,4 +3,13 @@
 class Blog extends AppModel {
 
     public $useDbConfig = 'blog';
+
+    /**
+     * Overridden paginateCount method
+     */
+    public function paginateCount($conditions = null, $recursive = 0, $extra = array()) {
+        $this->recursive = $recursive;
+        $results = $this->find('all');
+        return count($results);
+    }
 }
