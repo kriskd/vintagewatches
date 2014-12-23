7fd975c0117d46754b0ebf9b37892f754bedb5ce
Kris Dockter <kriskd@gmail.com>
2014-12-23 09:04:56 -0600

    Add auto prefixer to fix twbs carousel

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+var autoprefixer = require('autoprefixer-core');
 module.exports = function(grunt) {
 	'use strict';
 
@@ -49,14 +50,14 @@ module.exports = function(grunt) {
 			}
 		},
         watch: {
-			jss: {
-              files: ['webroot/js/src/*.js'],
+          jss: {
+            files: ['webroot/js/src/*.js'],
                tasks: ['concat', 'uglify']
-               },
-               css: {
+             },
+             css: {
                files: ['webroot/css/src/styles.less'],
-               tasks: ['less', 'cssmin']
-               }
+               tasks: ['less', 'cssmin', 'postcss']
+           }
        },
        bowercopy: {
          options: {
@@ -64,15 +65,23 @@ module.exports = function(grunt) {
             destPrefix: 'webroot/css'
           },
           fonts: 'bootstrap/fonts'
-      }
+      },
+      postcss: {
+        options: {
+          processors: [
+            autoprefixer({ browsers: ['last 2 version'] }).postcss
+          ]
+        },
+        dist: { src: 'webroot/css/build/*.css' }
+      },
    });
 
-               grunt.loadNpmTasks('grunt-contrib-concat');
-               grunt.loadNpmTasks('grunt-contrib-uglify');
-               grunt.loadNpmTasks('grunt-contrib-cssmin');
-               grunt.loadNpmTasks('grunt-contrib-watch');
-               grunt.loadNpmTasks('grunt-contrib-less');
-                grunt.loadNpmTasks('grunt-bowercopy');
-
-               grunt.registerTask('default', ['concat:dist', 'uglify']);
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-contrib-less');
+   grunt.loadNpmTasks('grunt-bowercopy');
+   grunt.loadNpmTasks('grunt-postcss');
+   grunt.registerTask('default', ['concat:dist', 'uglify']);
 };
