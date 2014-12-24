11828f462ea92e0b5e1732db56a5b2475f64941b
Kris Dockter <kriskd@gmail.com>
2014-12-24 06:34:32 -0600

    Add titles remove unused index method

--- Controller/BlogsController.php
+++ Controller/BlogsController.php
@@ -6,20 +6,6 @@ class BlogsController extends AppController {
 
     public $cacheAction = "1 day";
 
-    public $paginate = array(
-        'limit' => 5,
-        'order' => array(
-            'Blog.published' => 'desc'
-        )
-    );
-    
-    public function index() {
-        $this->Paginator->settings = $this->paginate;
-        $blogs = $this->Paginator->paginate('Blog');
-        //debug($blogs);
-        $this->set('blogs', $blogs);
-    }
-
     public function view($id = null) {
         if (empty($id)) {
             $blog = $this->Blog->find('first', array(
@@ -47,6 +33,7 @@ class BlogsController extends AppController {
             }
             $this->redirect($this->here);
         }
+        $this->set('title', $blog['Blog']['title']);
         $this->set(compact('blog', 'blogIndex'));
     }
 }
