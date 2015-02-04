765bcd2785887c1f6fd6ddd591aac681154fcfa3
Kris Dockter <kriskd@gmail.com>
2015-02-04 17:18:17 -0600

    Test orders method

--- Test/Case/Controller/WatchesControllerTest.php
+++ Test/Case/Controller/WatchesControllerTest.php
@@ -1,51 +1,45 @@
 <?php
 App::uses('WatchesController', 'Controller');
+App::uses('CakeRequest', 'Network');
+App::uses('ComponentCollection', 'Controller');
+App::uses('SessionComponent', 'Controller/Component');
 
 class WatchesControllerTest extends ControllerTestCase {
     
-    public $fixtures = array(
-        'app.order',
-        'app.watch',
-        'app.brand',
-        'app.image',
-        'app.payment',
-        'app.address',
-    );
-    public function setUp() {
-        $Watches = $this->generate('Watches', array(
-            'components' => array(
-                'Auth' => array('user'),
-            )   
-        ));
-
-        /*$newUserData = array(
-            'User' => array(
-                'username' => 'user',
-                'password' => 'password',
-            )
-        );
-        $mock = $this->generate('Watches', array(
-            'components' => array(
-                'Session'
-            )
-        ));
-        $mapCheck = array(
-            array('NewUserData', true)
-        );
-        $mapRead = array(
-            array('NewUserData', $newUserData)
-        );
-        
-        $mock->Session->expects($this->any())
-            ->method('check')
-            ->will($this->returnValueMap($mapCheck));
-        
-        $mock->Session->expects($this->any())
-            ->method('read')
-            ->will($this->returnValueMap($mapRead));*/
-    }    
+	public $fixtures = array(
+		'app.watch',
+		'app.order',
+		'app.coupon',
+		'app.brand',
+		'app.payment',
+		//'app.invoice',
+		//'app.invoice_item',
+		'app.address',
+		'app.detect',
+		'app.detects_order',
+		'app.image',
+		'app.state',
+		'app.province',
+		'app.country',
+		'app.page',
+		//'app.content'
+	);
+    
     public function testIndex() {
         $result = $this->testAction('/watches/index');
         debug($result);
+    }
+
+    public function testOrder() {
+        $this->ComponentCollection = new ComponentCollection();
+        $Session = new SessionComponent($this->ComponentCollection);
+
+        $Session->write('Order.email', 'PeterRHarris@teleworm.us');
+        $Session->write('Address.postalCode', '61602');
+        $results = $this->testAction('/watches/order/1', array(
+            'method' => 'GET',
+            'return' => 'vars',
+        ));
+        $this->assertNotEmpty($results['title']);
     }
 }
