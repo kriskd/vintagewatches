5335b6468665826bbd13aaa2df8e697f9faae5ca
Kris Dockter <kriskd@gmail.com>
2015-02-10 17:16:04 -0600

    Generate with cake bake

--- Test/Fixture/PageFixture.php
+++ Test/Fixture/PageFixture.php
@@ -1,9 +1,34 @@
 <?php
-
+/**
+ * PageFixture
+ *
+ */
 class PageFixture extends CakeTestFixture {
-    public $useDbConfig = 'test';
-    public $import = array(
-        'model' => 'Page',
-        'records' => true,
-    );
+
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('records' => true);
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'slug' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'homepage' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
 }
