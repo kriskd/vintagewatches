31ed12ba3d2c0c369f159c167083059a4abe0ca3
Kris Dockter <kriskd@gmail.com>
2016-07-10 15:56:49 -0500

    New fixtures

---
+++ Test/Fixture/ItemFixture.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ItemFixture
+ *
+ */
+class ItemFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'description' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'quantity' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 10, 'unsigned' => true),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'description' => 'Lorem ipsum dolor sit amet',
+			'quantity' => 1
+		),
+	);
+
+}
---
+++ Test/Fixture/ShippingFixture.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ShippingFixture
+ *
+ */
+class ShippingFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'description' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'amount' => array('type' => 'float', 'null' => false, 'default' => '0.00', 'length' => '6,2', 'unsigned' => false),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'description' => 'Lorem ipsum dolor sit amet',
+			'amount' => 1
+		),
+	);
+
+}
