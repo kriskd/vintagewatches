72f7d73dc5215aa5a502a4b1307bd5251c61a091
Kris Dockter <kriskd@gmail.com>
2016-07-10 17:19:32 -0500

    Re-generate fixture

--- Test/Fixture/ItemFixture.php
+++ Test/Fixture/ItemFixture.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * ItemFixture
- *
+ * Item Fixture
  */
 class ItemFixture extends CakeTestFixture {
+
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('connection' => 'development');
 
 /**
  * Fields
@@ -12,8 +18,12 @@ class ItemFixture extends CakeTestFixture {
  */
 	public $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => null),
 		'description' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'quantity' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 10, 'unsigned' => true),
+		'price' => array('type' => 'float', 'null' => false, 'default' => null, 'length' => '6,2', 'unsigned' => false),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
@@ -27,9 +37,22 @@ class ItemFixture extends CakeTestFixture {
  */
 	public $records = array(
 		array(
-			'id' => 1,
-			'description' => 'Lorem ipsum dolor sit amet',
-			'quantity' => 1
+			'id' => '1',
+			'active' => 1,
+			'description' => 'Book',
+			'quantity' => '99',
+			'price' => '49.95',
+			'created' => '2016-07-04 18:13:04',
+			'modified' => '2016-07-04 19:04:48'
+		),
+		array(
+			'id' => '2',
+			'active' => 1,
+			'description' => 'Knives',
+			'quantity' => '99',
+			'price' => '29.99',
+			'created' => '2016-07-04 23:57:25',
+			'modified' => '2016-07-05 00:01:23'
 		),
 	);
 
