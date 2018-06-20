218744b19a65d4a09481cc9c21d81cbe4a864258
Kris Dockter <kriskd@gmail.com>
2018-06-20 16:53:59 -0500

    Add selector on sold page to submit filter option

--- webroot/js/src/script.js
+++ webroot/js/src/script.js
@@ -165,7 +165,7 @@ $(document).ready(function(){
         setCookie('hideWatchIntro', 0, 90);
     });
 
-    $(document).on('change', '#WatchAdminIndexForm, #WatchIndexForm', function(){
+    $(document).on('change', '#WatchAdminIndexForm, #WatchIndexForm, #WatchSoldForm', function(){
         $(this).submit();
     });
 
